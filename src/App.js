64ccac707a12ea81da19d5f4ffd83aa3b4b776ec
ekeskn <ege-keskintas@hotmail.com>
2023-11-17 14:23:46 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Weather from './components/Weather';
+import { ThemeProvider } from './context/ThemeContext';
+
+function App() {
+  return (
+    <>
+      <ThemeProvider>
+        <Weather/>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
